3ccfae774132a03fd14d419018e78e5e9f6e1c80
Sven Liebig <liebigsv@gmail.com>
2017-05-19 17:28:10 +0200

    add typescript full support

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,33 @@
 var gulp = require('gulp');
 var util = require('gulp-util');
+var tsc = require('gulp-typescript');
+var sourcemaps = require('gulp-sourcemaps');
 var runSequence = require('run-sequence');
-
 
 function handleError(err) {
     console.log(err.toString());
     this.emit('end');
 }
+
+gulp.task('compile-ts', ['compile-src', 'compile-test']);
+
+gulp.task('compile-test', function () {
+	var tsProject = tsc.createProject('./src/tsconfig.json');
+    return gulp.src(['./e2e/*.ts'])
+        .pipe(tsProject())
+        .on('error', util.log)
+        .pipe(gulp.dest('./e2e/'));
+});
+
+gulp.task('compile-src', function () {
+	var tsProject = tsc.createProject('./src/tsconfig.json');
+    return gulp.src(['./src/app/**/*.ts'])
+		.pipe(sourcemaps.init())
+        .pipe(tsProject())
+        .on('error', util.log)
+		.pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./src/app/'));
+});
 
 gulp.task('pack', ['pack-src']);
 
