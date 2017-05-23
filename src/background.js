32e711bc8b8adc3caf8d7d2040f2118ddeaadefc
Sven Liebig <liebigsv@gmail.com>
2017-05-23 18:28:41 +0200

    update logger output

--- src/background.js
+++ src/background.js
@@ -2,67 +2,74 @@
 // in config/env_xxx.json file.
 import env from './env';
 
-import path from 'path';
-import url from 'url';
-import { app, Menu } from 'electron';
-import { devMenuTemplate } from './menu/dev_menu_template';
-import { editMenuTemplate } from './menu/edit_menu_template';
-import createWindow from './helpers/window';
 import './helpers/logger';
 
-const setApplicationMenu = () => {
-  const menus = [editMenuTemplate];
-  if (env.name !== 'production') {
-    menus.push(devMenuTemplate);
-  }
-  Menu.setApplicationMenu(Menu.buildFromTemplate(menus));
-};
+import path from 'path';
+import url from 'url';
+import {
+	app,
+	Menu
+} from 'electron';
+import {
+	devMenuTemplate
+} from './menu/dev_menu_template';
+import {
+	editMenuTemplate
+} from './menu/edit_menu_template';
 
-let mainWindow;
+import createWindow from './helpers/window';
+
+const setApplicationMenu = () => {
+	const menus = [editMenuTemplate];
+	if (env.name !== 'production') {
+		menus.push(devMenuTemplate);
+	}
+	Menu.setApplicationMenu(Menu.buildFromTemplate(menus));
+};
 
 // Save userData in separate folders for each environment.
 // Thanks to this you can use production and development versions of the app
 // on same machine like those are two separate apps.
 if (env.name !== 'production') {
-  const userDataPath = app.getPath('userData');
-  app.setPath('userData', `${userDataPath} (${env.name})`);
+	const userDataPath = app.getPath('userData');
+	app.setPath('userData', `${userDataPath} (${env.name})`);
 }
 
 app.on('ready', () => {
-  setApplicationMenu();
+	setApplicationMenu();
 
-  mainWindow = createWindow('main', {
-    width: 600,
-    height: 100,
-	frame: false,
-	skipTaskbar: false,
-	fullscreen: false,
-	transparent: true,
-	movable: true,
-	resizable: false,
-	alwaysOnTop: true
-	// macos
-	//titleBarStyle: 'hidden-inset'
-  });
+	let mainWindow = createWindow('main', {
+		width: 600,
+		height: 100,
+		frame: false,
+		skipTaskbar: false,
+		fullscreen: false,
+		transparent: true,
+		movable: true,
+		resizable: false,
+		alwaysOnTop: true
+		// macos
+		//titleBarStyle: 'hidden-inset'
+	});
 
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'app.html'),
-    protocol: 'file:',
-    slashes: true,
-  }));
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'app.html'),
+		protocol: 'file:',
+		slashes: true,
+	}));
 
-  mainWindow.on('blur', () => {
-	 console.log('mainwindow > event > onblur');
-	 mainWindow.hide();
-  });
+	mainWindow.on('blur', () => {
+		global.logger.info("window - blur()");
+		mainWindow.hide();
+	});
 
-  app.window = mainWindow;
+	app.window = mainWindow;
 
-  if (env.name === 'development') {
-    mainWindow.openDevTools();
-  }
+	if (env.name === 'development') {
+		mainWindow.openDevTools();
+	}
 });
 
 app.on('window-all-closed', () => {
-  app.quit();
+	app.quit();
 });
--- src/helpers/logger.js
+++ src/helpers/logger.js
@@ -7,9 +7,34 @@ import env from '../env';
 
 const logger = require('winston');
 
-//logger
+function getTimestamp() {
+	let now = new Date();
+	let day = now.getDate() > 9 ? now.getDate() : "0" + now.getDate();
+	let month = (now.getMonth() + 1) > 9 ? (now.getMonth() + 1) : "0" + (now.getMonth() + 1);
+	let year = now.getYear();
+	let hours = now.getHours() > 9 ? now.getHours() : "0" + now.getHours();
+	let minutes = now.getMinutes() > 9 ? now.getMinutes() : "0" + now.getMinutes();
+	let seconds = now.getSeconds() > 9 ? now.getSeconds() : "0" + now.getSeconds();
+	return `[${day}.${month}.${year}-${hours}:${minutes}:${seconds}] -`;
+}
+
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+	timestamp: () => {
+		return getTimestamp();
+	},
+	formatter: function(options) {
+		// Return string will be passed to logger.
+		return options.timestamp() +' '+ options.level.toUpperCase() +' '+ (options.message ? options.message : '') +
+		(options.meta && Object.keys(options.meta).length ? '\n\t'+ JSON.stringify(options.meta) : '' );
+	},
+	colorize: true,
+});
 
 logger.add(logger.transports.File, {
+	timestamp: () => {
+		return getTimestamp();
+	},
 	name: 'error-file',
 	filename: './logs/logs/error.log',
 	level: 'error',
@@ -17,6 +42,9 @@ logger.add(logger.transports.File, {
 });
 
 logger.add(logger.transports.File, {
+	timestamp: () => {
+		return getTimestamp();
+	},
 	name: 'debug-file',
 	filename: './logs/debug.log',
 	level: 'debug',
@@ -24,16 +52,13 @@ logger.add(logger.transports.File, {
 });
 
 logger.add(logger.transports.File, {
+	timestamp: () => {
+		return getTimestamp();
+	},
 	name: 'info-file',
 	filename: './logs/info.log',
 	level: 'info',
 	json: false
-});
-
-logger.addColors({
-	debug: "yellow",
-	error: "red",
-	info: "blue"
 });
 
 /*
@@ -43,6 +68,6 @@ logger.add(logger.transports.File, { filename: path.join(logFolder, logFile) });
 
 global.logger = logger;
 
-if (env.name == 'development') {
+if (env.name === 'development') {
 	logger.level = 'debug';
 }
